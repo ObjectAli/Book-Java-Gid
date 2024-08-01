8ff3cfac1508e7b35821fdef470a0a277b5380f7
Ali N <norbobaev@gmail.com>
2024-08-01 18:03:19 +0300

    chapter 4 - final

--- documents-management-system/src/main/java/org/api/doc/manag/system/Main.java
+++ documents-management-system/src/main/java/org/api/doc/manag/system/Main.java
@@ -1,7 +1,14 @@
 package org.api.doc.manag.system;
 
+import org.api.doc.manag.system.importer.Importer;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+
+    private final Map<String, Importer> extensionToImporter = new HashMap<>();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
